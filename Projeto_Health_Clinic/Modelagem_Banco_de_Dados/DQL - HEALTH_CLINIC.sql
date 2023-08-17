15afe0137e264dbce7603e2ac498257cc0bfbfb7
Vinicius Porcionato <porcionatovinicius@gmail.com>
2023-08-17 15:09:29 -0300

    Termino do BD Projeto Health Clinic

--- Projeto_Health_Clinic/Modelagem_Banco_de_Dados/DQL - HEALTH_CLINIC.sql
+++ Projeto_Health_Clinic/Modelagem_Banco_de_Dados/DQL - HEALTH_CLINIC.sql
@@ -2,5 +2,22 @@
 
 USE Health_Clinic_TD
 
-SELECT 
-FROM
+SELECT Consulta.IdConsulta,
+Consulta.DataConsulta,
+Consulta.HoraConsulta,
+Clinica.NomeFantasia,
+P.Nome AS NomePaciente, 
+M.Nome AS NomeMedico,
+Especialidade.TituloEspecialidade AS EspecialidadeMedico,
+Medico.CRM,
+Consulta.Descricao AS Prontuario,
+Comentario.Descricao AS Feedback
+
+FROM Consulta
+INNER JOIN Paciente ON Consulta.IdPaciente = Paciente.IdPaciente
+INNER JOIN Medico ON Consulta.IdMedico = Medico.IdMedico
+INNER JOIN Usuario M ON Medico.IdUsuario = M.IdUsuario
+INNER JOIN Usuario P ON Paciente.IdUsuario = P.IdUsuario
+INNER JOIN Especialidade ON Medico.IdEspecialidade = Especialidade.IdEspecialidade
+INNER JOIN Clinica ON Consulta.IdClinica = Clinica.IdClinica
+LEFT JOIN Comentario ON Paciente.IdPaciente = Comentario.IdPaciente
