c78025f4a427e94a5f0d0b8a81dd44917c8a2d61
Vinicius Porcionato <porcionatovinicius@gmail.com>
2023-09-19 17:17:11 -0300

    Continuação da web api Event+

---
+++ Projeto_Event+/Event+_API_CF/webapi.event+.tarde/Controllers/TipoUsuarioController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webapi.event_.tarde.Domains;
+using webapi.event_.tarde.Interfaces;
+using webapi.event_.tarde.Repositories;
+
+namespace webapi.event_.tarde.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]  
+    public class TipoUsuarioController : ControllerBase
+    {
+        private ITipoUsuarioRepository _tipoUsuarioRepository { get; set; }
+
+        public TipoUsuarioController()
+        {
+            _tipoUsuarioRepository = new TipoUsuarioRepository();
+        }
+
+        [HttpPost("Cadastrar")]
+        [Authorize(Roles = "3FA85F64-5717-4562-B3FC-2C963F66AFA6")]
+        public IActionResult Post(TipoUsuario tipoUsuario)
+        {
+            try
+            {
+                _tipoUsuarioRepository.Cadastrar(tipoUsuario);
+                return StatusCode(201);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
---
+++ Projeto_Event+/Event+_API_CF/webapi.event+.tarde/Utils/Criptografia.cs
@@ -0,0 +1,15 @@
+﻿namespace webapi.event_.tarde.Utils
+{
+    public static class Criptografia
+    {
+        public static string GerarHash(string senha)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(senha);
+        }
+
+        public static bool CompararHash(string senhaForm, string senhaBD)
+        {
+            return BCrypt.Net.BCrypt.Verify(senhaForm, senhaBD);
+        }
+    }
+}
