bace2c5e7a7444bdc15e325d434861bc8eaa4847
Vinicius Porcionato <porcionatovinicius@gmail.com>
2023-09-28 17:18:33 -0300

    Continuação Web API HEalth CLinic

--- Projeto_Health_Clinic/webapi.healthclinic.tarde/Interfaces/IConsultaRepository.cs
+++ Projeto_Health_Clinic/webapi.healthclinic.tarde/Interfaces/IConsultaRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace webapi.healthclinic.tarde.Interfaces
+﻿using webapi.healthclinic.tarde.Domains;
+
+namespace webapi.healthclinic.tarde.Interfaces
 {
     public interface IConsultaRepository
     {
+        void Cadastrar(Consulta consulta);
+        void Deletar(Guid id);
+        void Atualizar(Guid id, Consulta consulta);
+        Consulta BuscarPorId(Guid id);
     }
 }
---
+++ Projeto_Health_Clinic/webapi.healthclinic.tarde/Repositories/ConsultaRepository.cs
@@ -0,0 +1,79 @@
+﻿using webapi.healthclinic.tarde.Context;
+using webapi.healthclinic.tarde.Domains;
+using webapi.healthclinic.tarde.Interfaces;
+
+namespace webapi.healthclinic.tarde.Repositories
+{
+    public class ConsultaRepository : IConsultaRepository
+    {
+        private readonly HealthClinicContext _healthClinicContext;
+
+        public ConsultaRepository()
+        {
+            _healthClinicContext = new HealthClinicContext();
+        }
+
+        public void Atualizar(Guid id, Consulta consulta)
+        {
+            Consulta consultaBuscada = _healthClinicContext.Consulta.Find(id)!;
+
+            if (consultaBuscada != null)
+            {
+
+                consultaBuscada.DataConsulta = consulta.DataConsulta;
+                consultaBuscada.HoraConsulta = consulta.HoraConsulta;
+
+            }
+
+            _healthClinicContext.Consulta.Update(consultaBuscada!);
+            _healthClinicContext.SaveChanges();
+        }
+
+        public Consulta BuscarPorId(Guid id)
+        {
+            try
+            {
+                return _healthClinicContext.Consulta.FirstOrDefault(e => e.IdConsulta == id)!;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void Cadastrar(Consulta consulta)
+        {
+            try
+            {
+                _healthClinicContext.Consulta.Add(consulta);
+                _healthClinicContext.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void Deletar(Guid id)
+        {
+            try
+            {
+                Consulta consultaBuscada = _healthClinicContext.Consulta.Find(id)!;
+
+                if (consultaBuscada != null)
+                {
+                    _healthClinicContext.Consulta.Remove(consultaBuscada);
+                }
+
+                _healthClinicContext.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
